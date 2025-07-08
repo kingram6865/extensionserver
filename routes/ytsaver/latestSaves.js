91d68bcc263957ea544496bb8aa9c41409e7504e
Ken Ingram <ken.ingram@gmail.com>
2025-07-07 21:15:51 -0700

    Prepare SQL for no start or end; default to NOW();

--- routes/ytsaver/latestSaves.js
+++ routes/ytsaver/latestSaves.js
@@ -5,13 +5,24 @@ export const latestSaves = {
   path: "/ytsave",
   method: "get",
   handler: async (req, res) => {
+    let sql
     // Select list of saved links between start and end date. End date is optional (defaults to NOW())
     const [start, end] = req.params;
     // Make sure query parameters are dates
 
-    const oldest = formatDateString(start)
-    const latest = formatDateString(end)
-    const sql = "SELECT * from youtube_downloads WHERE entry_date >= ? AND entry_date <= ?"
+    const oldest = formatDateString(start);
+    const latest = formatDateString(end);
+    
+    // if (oldest && latest) {
+    //   sql = "SELECT * from youtube_downloads WHERE entry_date >= ? AND entry_date <= ?"
+    // } else if (oldest) {
+    //   sql = "SELECT * from youtube_downloads WHERE entry_date >= ? AND entry_date <= ?"
+    // } else if (latest) {
+    //   sql = "SELECT * from youtube_downloads WHERE entry_date >= ? AND entry_date <= ?"
+    // } else {
+    //   sql = "SELECT * from youtube_downloads WHERE entry_date >= ? AND entry_date <= ?"
+    // }
+    sql = "SELECT * from youtube_downloads WHERE entry_date >= ? AND entry_date <= ?"
 
     try {
       const results = await executeSQL(sql, test, 55, [oldest, latest]);
