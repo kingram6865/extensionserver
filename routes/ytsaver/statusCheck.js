03811a1c8fac24a71e65d0c51798161bfc9fd2f6
Ken Ingram <ken.ingram@gmail.com>
2025-07-09 17:51:59 -0700

    Create a route to check connection status.

--- routes/ytsaver/statusCheck.js
+++ routes/ytsaver/statusCheck.js
@@ -4,8 +4,8 @@ export const statusCheck = {
   path: "/ytsaver/status",
   method: "post",
   handler: async (req, res) => {
-    const SQL = 'SELECT objid, caption, description FROM youtube_downloads WHERE videoid = ?';
-    let result = await executeSQL(SQL, 'random_facts', 55, [req.videoid]);
-    return res.json(result);
+    const SQL = "SELECT objid, caption, description FROM youtube_downloads WHERE videoid = ?";
+    let result = await executeSQL(SQL, 'random_facts', 55, [req.body.videoid]);
+    res.json(result.data[0])
   }
 }
