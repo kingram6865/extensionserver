326dc8933c06c4026f645fa17d60d1ab078cf0ab
Ken Ingram <ken.ingram@gmail.com>
2025-09-02 11:42:22 -0700

    Create route to add video data to database (completed 2025-08-16 16:00)

--- routes/ytsaver/saveVideoLink.js
+++ routes/ytsaver/saveVideoLink.js
@@ -4,8 +4,16 @@ import { validateVideoId } from "../../utilities/youtube";
 export const saveVideoLink = {
   path: "/ytsaver/save",
   method: "post",
-  handler: async (req, res) => {
-    let apiVideoData = await validateVideoId(req.body)
-    return res.json(apiVideoData);
+  handler: async (req, res, next) => {
+    let apiVideoData
+    // req.log?.info('ytsaver.save.ok', { videoId: apiVideoData?.id });
+    try {
+      apiVideoData = await validateVideoId(req.body)
+      // req.log?.info('ytsaver.save.ok', { videoId: apiVideoData?.id });
+      return res.json(apiVideoData);
+    } catch (err) {
+      req.log?.error('ytsaver.save.fail', { error: err.message });
+      next(err);
+    }
   }
 }
